35a525ad874b21e097500044b3e95390bd82a356
Maegan <maegan@maeganwilson.com>
2021-10-29 06:25:09 -0500

    Initial Commit

---
+++ halloween-challenge/halloween_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  halloween_challengeApp.swift
+//  halloween-challenge
+//
+//  Created by Maegan Wilson on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct halloween_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
